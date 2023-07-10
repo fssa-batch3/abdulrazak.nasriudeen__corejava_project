1c0a0d4fe617149b1a5828306b8ed51f1d03f446
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-07-10 14:38:23 +0530

    intial commit

--- src/main/java/day01/solved/package2/AccessBicycle.java
+++ src/main/java/day01/solved/package2/AccessBicycle.java
@@ -1,10 +1,19 @@
 package day01.solved.package2;
 
+
+import day01.solved.package1.Bicycle;
+
+
 public class AccessBicycle {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Bicycle bike1 = new Bicycle();
+//		System.out.println(bike1.gear);
+		bike1.getGear();
+		
 	}
 
 }
